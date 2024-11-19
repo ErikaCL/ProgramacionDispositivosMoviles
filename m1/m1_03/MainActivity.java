47269d828010b1dc8b23817f66b2e68064bf312b
ErikaCL <129238025+ErikaCL@users.noreply.github.com>
2024-11-18 19:12:02 -0600

    Update MainActivity.java

--- m1/m1_03/MainActivity.java
+++ m1/m1_03/MainActivity.java
@@ -8,34 +8,28 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Crear un arreglo de 1 millón de elementos enteros aleatorios
         int[] numeros = new int[1_000_000];
         Random random = new Random();
 
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt(1_000_000);  // Rango de 0 a 999999
+            numeros[i] = random.nextInt(1_000_000);  
         }
 
-        // Medir el tiempo de inicio
         long tiempoInicio = System.currentTimeMillis();
 
         // Ordenar el arreglo
         Arrays.sort(numeros);
 
-        // Medir el tiempo de fin
         long tiempoFin = System.currentTimeMillis();
 
-        // Calcular el tiempo transcurrido
         long tiempoTranscurrido = tiempoFin - tiempoInicio;
 
         // Imprimir el tiempo de ejecución en el logcat
-        Log.d(TAG, "Tiempo de ejecución en milisegundos: " + tiempoTranscurrido);
+        Log.d("MainActivity", "Tiempo Total de Ejecución (en milisegundos): " + tiempoTranscurrido);
     }
 }
